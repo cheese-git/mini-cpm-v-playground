3f6d8b85083eb028ba243aaf3da4b5a609f5b4c1
Cheese <11363971+cheese-git@users.noreply.github.com>
2024-04-12 13:48:28 +0800

    feat: Output latency

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import time
 import gradio as gr
 
-def infer(img, question):
-    print(img)
 
-    return "1"
+def infer(img, question):
+    start_time = time.time()
+    print(img)
+    latency = time.time() - start_time
+
+    return "1", latency
 
 
 demo = gr.Interface(
@@ -11,7 +15,10 @@ demo = gr.Interface(
     description="A playground for MiniCPM-V",
     fn=infer,
     inputs=[gr.Image(label="Image"), gr.Textbox(label="Question")],
-    outputs="text",
+    outputs=[
+        gr.Textbox(label="Answer"),
+        gr.Number(label="Latency(s)"),
+    ],
     article="""[https://github.com/OpenBMB/MiniCPM-V](https://github.com/OpenBMB/MiniCPM-V)""",
 )
 
