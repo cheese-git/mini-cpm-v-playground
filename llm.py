6695490c18417aa5fede9ba4a808542e33c920fa
Cheese <11363971+cheese-git@users.noreply.github.com>
2024-04-16 10:21:43 +0800

    config device types

--- llm.py
+++ llm.py
@@ -6,11 +6,12 @@ load_dotenv()
 import torch
 from chat import OmniLMMChat, img2base64
 
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+dtype = torch.float16 if device == "mps" else torch.bfloat16
 torch.manual_seed(0)
 
 model = OmniLMMChat("openbmb/OmniLMM-12B")
-# model = model.to(device="mps", dtype=torch.float16)
-
+model = model.to(device, dtype=dtype)
 
 def infer(img, question):
     """
